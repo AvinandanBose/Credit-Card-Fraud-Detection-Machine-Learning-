bb1b26b9a31adb92f77b203c7d242e65347eb35a
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-12-21 01:49:59 +0530

    Add files via upload

---
+++ Front end part 2/static/form.js
@@ -0,0 +1,37 @@
+function my_magic_function(val) {
+    //modal.find('.modal-title').text('New message to ' + var);
+    console.log(val);
+    document.getElementById("result").innerHTML = "Status : " + val.toString();
+}
+
+$(document).ready(function () {
+    $('form').on('submit', function (event) {
+        $.ajax({
+            data: JSON.stringify({
+                Amount: $('#amountInput').val(),
+                Time: $('#timeInput').val()
+            }),
+            type: 'POST',
+            url: '/process',
+            dataType: 'json',
+            success: function (response) {
+
+                value = response.message;
+
+                my_magic_function(value);
+
+            }
+        })
+
+        error: (error) => {
+            console.log(JSON.stringify(error));
+        }
+        event.preventDefault();
+
+    });
+});
+
+
+function onClickedSubmitBtn() {
+    console.log("Submit button clicked");
+}
